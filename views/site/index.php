09ae7d2f0b11703bd7b9f18e46da0ecb1f2bd84c
Web <georgrussib@gmail.com>
2022-10-21 15:55:16 +0700

    Commit_4

--- views/site/index.php
+++ views/site/index.php
@@ -1,8 +1,8 @@
 <?php
 
 /* @var $this yii\web\View */
-
 $this->title = 'Fotocenter';
+// Путь к картинкам указывать относительно корневой папки
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +12,16 @@ $this->title = 'Fotocenter';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fotocenter</title>
+    <style type="text/css">
+        img {
+            width: 200px;
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
     <h1>Hello World!!!</h1>
+    <img class="Html" src="/views/site/img/!bf4.jpg" alt="">
+    <img class="Html" src="/views/site/img/++.jpg" alt="">
 </body>
 </html>
